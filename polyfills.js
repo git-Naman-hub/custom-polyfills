b2701d26ab3cee7272dd9bbd9c4ab42516bff1cc
naman <nsnaman9729@gmail.com>
2026-03-02 02:48:12 +0530

    add custom reduce

--- polyfills.js
+++ polyfills.js
@@ -37,3 +37,16 @@ Array.prototype.custom_filter = function (someFunction){
 
 // const myArr = [1,2,3,4,5,6,7,8,9]
 // console.log(myArr.custom_filter((value)=>(value>5)))
+
+//my own reduce
+
+Array.prototype.custom_reduce = function (someFunction,initialValue){
+    let acc = initialValue
+    for(let i=0;i<this.length;i++){
+        acc=someFunction(acc,this[i],i,this)
+    }
+    return acc
+}
+
+// const myArr = ["BA","NA","NA"]
+// console.log(myArr.custom_reduce((acc,val)=>(acc+val),""))
