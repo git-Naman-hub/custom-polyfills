abfb281070f91dc77b6d891bb201ab6ee820e591
naman <nsnaman9729@gmail.com>
2026-03-02 02:19:21 +0530

    add custom map

--- polyfills.js
+++ polyfills.js
@@ -9,3 +9,18 @@ Array.prototype.custom_forEach = function (someFunction){
 
 // const myArr = ["italian","indian","mexican"]
 // myArr.custom_forEach((val)=>(console.log(val+" cuisine")))
+
+//my own map
+
+Array.prototype.custom_map = function (someFunction){
+    const result = []
+    for(let i = 0;i<this.length;i++){
+        result.push(someFunction(this[i],i,this))
+    }
+    return result
+}
+
+// const myArr = [1,2,3]
+// console.log(myArr.custom_map((val)=>(val*10)))
+
+//my own filter
